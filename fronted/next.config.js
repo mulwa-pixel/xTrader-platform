9bf42371d70d95a56555706cae0bc48b4e0b7e78
mulwa-pixel <barlinparlemo@gmail.com>
2026-02-16 08:34:10 +0300

    Update next.config.js

--- fronted/next.config.js
+++ fronted/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['deriv.com'],
   },
 }
 
